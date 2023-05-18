f2daa5572a443c0e46baa33e8a0d1406fe6ec9f7
pinky-pig <2545589993@qq.com>
2023-05-18 13:25:58 +0800

    feat: 添加 express

--- server.mjs
+++ server.mjs
@@ -1,6 +1,22 @@
-import { createServer } from 'http';
+import express from 'express';
+import PreviewRoute from './api/previewSite.js'
+const app = express()
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+// axios接口数据。axios数据请求方式x-www-form-urlencoded
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/preview', PreviewRoute)
+
+app.all("*",function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","content-type");
+  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+    res.send(200);  //让options尝试请求快速结束
+  else
+    next();
+})
+
+app.listen(process.env.PORT || 3200, (req, res) => {
+  console.log('express start port 3200');
+})
